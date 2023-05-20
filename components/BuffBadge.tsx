4a58ab46e9f7f9734955e8720ab6773a704ec344
r4bb1t <r4bb1t.github@gmail.com>
2023-05-20 11:25:25 +0900

    badge color

--- components/BuffBadge.tsx
+++ components/BuffBadge.tsx
@@ -14,7 +14,7 @@ export default function BuffBadge({
     <div
       className={cc([
         "tooltip text-sm border-2 border-black px-1 py-0.5 text-white bg-black rounded",
-        buff.month + buff.duration < month && "!opacity-50",
+        buff.month + buff.duration < month && "!bg-opacity-50",
       ])}
       data-tip={buff.description
         .replace("%grade", `${buff.grade}`)
